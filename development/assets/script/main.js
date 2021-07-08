48b8590f48f1872dc5396f0b92b5c4dfc7b83075
Ozan Ali TÜRKYILMAZ <74865639+OzanTurkyilmaz@users.noreply.github.com>
2021-07-08 20:00:18 +0300

    Add files via upload

---
+++ development/assets/script/main.js
@@ -0,0 +1,373 @@
+let data = [
+	{
+		"id": 1,
+		"name": "Ahmed Bustani",
+		"assistant": true,
+		"group": "Orange"
+	},
+	{
+		"id": 2,
+		"name": "Baris Tunar",
+		"assistant": true,
+		"group": "Crimson"
+	},
+	{
+		"id": 3,
+		"name": "Burak Caglar",
+		"assistant": true,
+		"group": "Khaki"
+	},
+	{
+		"id": 4,
+		"name": "ah. fawad",
+		"assistant": true,
+		"group": "Pink"
+	},
+	{
+		"id": 5,
+		"name": "Furkan KILIÇER",
+		"assistant": true,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 6,
+		"name": "Alperen Aslan",
+		"assistant": true,
+		"group": "Turquoise"
+	},
+	{
+		"id": 7,
+		"name": "joudi bitar",
+		"assistant": true,
+		"group": "Yellow"
+	},
+	{
+		"id": 8,
+		"name": "Kübra Özdem",
+		"assistant": true,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 9,
+		"name": "Aycan",
+		"assistant": true,
+		"group": "Fuchsia"
+	},
+	{
+		"id": 10,
+		"name": "Aykut Şahin",
+		"assistant": true,
+		"group": "Puce"
+	},
+	{
+		"id": 11,
+		"name": "S. Zahit ACAR",
+		"assistant": true,
+		"group": "Maroon"
+	},
+	{
+		"id": 12,
+		"name": "Tugay Türk",
+		"assistant": true,
+		"group": "Purple"
+	},
+	{
+		"id": 13,
+		"name": "Abdurrahman Seyidoğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 14,
+		"name": "Ahmed Tabşo",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 15,
+		"name": "Alaa Alaff",
+		"assistant": false,
+		"group": "Fuchsia"
+	},
+	{
+		"id": 16,
+		"name": "ASSKOUR Imane",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 17,
+		"name": "Aydoğan Gökberk",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 18,
+		"name": "BAHADDİN ALMUHAMMED",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 19,
+		"name": "Büşra Bilgin",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 20,
+		"name": "Chimnaz Vahabzade",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 21,
+		"name": "Çağatay Sarıoğlan",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 22,
+		"name": "dana alhameed",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 23,
+		"name": "Delfin Öztürk",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 24,
+		"name": "Ebubekir Karataş",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 25,
+		"name": "Ela Yağmur ",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 26,
+		"name": "ELNAZ HAMEDIBAZAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 27,
+		"name": "Fatma Şehitoğlu",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 28,
+		"name": "Feras Alkilani",
+		"assistant": false,
+		"group": "Fuchsia"
+	},
+	{
+		"id": 29,
+		"name": "Gizem Nur Korkmaz",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 30,
+		"name": "Halil İbrahim Badem",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 31,
+		"name": "Hasan ALKHALAF",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 32,
+		"name": "Hatice Sürümlü",
+		"assistant": false,
+		"group": "Purple"
+	},
+	{
+		"id": 33,
+		"name": "IMAN ALABID",
+		"assistant": false,
+		"group": "Maroon"
+	},
+	{
+		"id": 34,
+		"name": "Işıl Özkaynak",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 35,
+		"name": "İbrahim Halil DÖŞ",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 36,
+		"name": "İlkay Çitak",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 37,
+		"name": "Kenan Mirzeyev",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 38,
+		"name": "Melisa Gürdal",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 39,
+		"name": "Merve Şahin",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 40,
+		"name": "MHD NABIL ARAR",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 41,
+		"name": "Muhammet BİLGİN",
+		"assistant": false,
+		"group": "Orange"
+	},
+	{
+		"id": 42,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Khaki"
+	},
+	{
+		"id": 43,
+		"name": "Ozan Ali TÜRKYILMAZ",
+		"assistant": false,
+		"group": "Pink"
+	},
+	{
+		"id": 44,
+		"name": "Özge Yurdusev",
+		"assistant": false,
+		"group": "Goldenrod"
+	},
+	{
+		"id": 45,
+		"name": "Parahat Nepesov",
+		"assistant": false,
+		"group": "Puce"
+	},
+	{
+		"id": 46,
+		"name": "Rined haj osman",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 47,
+		"name": "Serkan Çelik",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 48,
+		"name": "Sinem Nurioğlu",
+		"assistant": false,
+		"group": "Aquamarine"
+	},
+	{
+		"id": 49,
+		"name": "Şifa Hasani",
+		"assistant": false,
+		"group": "Turquoise"
+	},
+	{
+		"id": 50,
+		"name": "Yasemin Paşaoğlu",
+		"assistant": false,
+		"group": "Yellow"
+	},
+	{
+		"id": 51,
+		"name": "Yusuf sabbagh",
+		"assistant": false,
+		"group": "Crimson"
+	},
+	{
+		"id": 52,
+		"name": "Nurgeldi Atamyradov",
+		"assistant": false,
+		"group": "Yellow"
+	}
+];
+
+// console.log('👻', data)
+
+
+
+const students = data.filter((student) => student.assistant != true);
+//console.log(students)
+const assistant = data.filter(person => person.assistant === true)
+//console.log(assistant)
+
+// const filteredStudents = students.map(a => {
+// 	console.log(a)
+// 	const data3 = students.filter(b => {
+// 		console.log(b)
+// 		return b.name != a.name
+		
+// 	})
+// 	return data3
+// }) 
+// console.log(filteredStudents)
+
+let categories = data.reduce((r, { group, ...object }) => {
+	var temp = r.find(o => o.group === group);
+	if (!temp) r.push(temp = { group, member: [] });
+	temp.member.push(object);
+	return r;
+}, []);
+console.log(categories)
+
+
+categories.forEach(element => {
+	const cards = document.createElement("DIV")
+	const teamLeader = document.createElement("H1")
+	cards.className = "groups"
+	cards.id = element.group
+	cards.style.backgroundColor = element.group
+	document.querySelector(".main").appendChild(cards)
+	cards.appendChild(teamLeader)
+	assistant.map(person => {
+		if (person.group === cards.id) {
+			teamLeader.innerHTML = person.name.toUpperCase()
+		}
+	})
+});
+
+const allGroups = document.querySelectorAll(".groups")
+
+allGroups.forEach(team => {
+	students.map(stu => {
+		if (stu.group === team.id) {
+			const li = document.createElement("LI")
+			li.innerHTML = stu.name
+			team.appendChild(li)
+		}
+	})
+
+
+})
+
